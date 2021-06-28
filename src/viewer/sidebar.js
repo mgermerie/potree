994fc41987f8117f436e62722aadb9604aeb5b00
gchoqueux <gerald.choqueux@ign.fr>
2021-06-28 16:17:19 +0200

    chore(package): use es6 import for spectrum and jquery-ui.

--- src/viewer/sidebar.js
+++ src/viewer/sidebar.js
@@ -18,6 +18,8 @@ import {OrientedImage} from "../modules/OrientedImages/OrientedImages.js";
 import {Images360} from "../modules/Images360/Images360.js";
 
 import JSON5 from "../../libs/json5-2.1.3/json5.mjs";
+import "../../libs/spectrum/spectrum.js";
+import * as jqueryui from "../../libs/jquery-ui/jquery-ui.min.js";
 
 export class Sidebar{
 
